7b858e5cfd8ac6830bc3783dd4285b9e93b118a6
Rian Carlos <riancarlos.sl@gmail.com>
2021-07-22 20:29:23 -0300

    initial commit

---
+++ HelloWindows/HelloWindows/HelloMsg.cpp
@@ -0,0 +1,13 @@
+#include <windows.h>
+
+int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+	int a = 10;
+	int b = 30;
+	MessageBox(
+		NULL,
+		"Oi windows",
+		"Mensagem",
+		0
+	);
+}
